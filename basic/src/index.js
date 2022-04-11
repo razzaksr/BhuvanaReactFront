f8ca330568f191617c051c60f21e2bb200ca519d
razzaksr <razzaksr@gmail.com>
2022-04-11 11:59:23 +0530

    basic project created

---
+++ basic/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <>
+    <ul>
+      <li>hello</li>
+      <li>react</li>
+    </ul>
+  </>,
+  document.getElementById('root')
+);
