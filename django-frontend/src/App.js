fa3d95137450305a19bc68922b90e089e4a5821b
razzaksr <razzaksr@gmail.com>
2022-04-21 11:33:15 +0530

    new corp front end done

--- django-frontend/src/App.js
+++ django-frontend/src/App.js
@@ -4,18 +4,18 @@ import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import { Home, One, Two } from "./SampleComps";
 import { NewCorp } from "./NewCorporate";
 
+
 const App=()=>{
   return (
     <>
       <BrowserRouter>
         <Menu/>
         <Routes>
-          <Route path="/" exact element={<Home/>}/>
+          <Route path="/" exact element={<NewCorp/>}/>
           <Route path="/one" exact element={<One/>}/>
           <Route path="/two/:data" exact element={<Two/>}/>
         </Routes>
       </BrowserRouter>
-      <h1>Hai!!!!!!!!!!!!!</h1>
     </>
   );
 }
